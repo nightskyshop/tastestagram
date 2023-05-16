25c6271c9543e29ba6147d81adf425ff7f851cee
nightskyshop1023 <nightskyshop1023@naver.com>
2023-05-16 15:31:53 +0900

    Deploy Test 2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.css";
 
 function Header() {
